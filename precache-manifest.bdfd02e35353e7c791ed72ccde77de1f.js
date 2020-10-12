26cf72c0109eb73a453ec3f992d0f4b7e0a8c85c
davidelamorte <lamortedavide@gmail.com>
2020-10-12 21:19:26 +0100

    Updates

---
+++ precache-manifest.bdfd02e35353e7c791ed72ccde77de1f.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7f67d6fee27171ffd0088f3ecb3d7d4d",
+    "url": "/box-fight-fe/index.html"
+  },
+  {
+    "revision": "890850f8703eb2856d9c",
+    "url": "/box-fight-fe/static/css/main.c96713ed.chunk.css"
+  },
+  {
+    "revision": "acba92e4a16bee06adee",
+    "url": "/box-fight-fe/static/js/2.1f6a6366.chunk.js"
+  },
+  {
+    "revision": "cd8c43dd6205c3e20844ddb4d46a64f2",
+    "url": "/box-fight-fe/static/js/2.1f6a6366.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "890850f8703eb2856d9c",
+    "url": "/box-fight-fe/static/js/main.eafb08c7.chunk.js"
+  },
+  {
+    "revision": "1106e331af6b1943f910",
+    "url": "/box-fight-fe/static/js/runtime-main.62b58523.js"
+  }
+]);
